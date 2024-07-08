2fed6300a65bf603522e0eee6a87c6e49f5ff6a7
dnguyen <eonho6582@gmail.com>
2024-07-08 23:15:58 +0700

    featur: get detail user

--- routes/User.js
+++ routes/User.js
@@ -2,9 +2,28 @@ const router = require("express").Router();
 const User = require("../model/User");
 
 router.get("/", async (req, res) => {
-  const data = await User.find();
-  console.log("responese get list user: ", JSON.stringify(data));
-  res.status(200).send(data);
+  try {
+    const data = await User.find();
+    console.log("responese get list user: ", JSON.stringify(data));
+    res.status(200).send(data);
+  } catch (error) {
+    console.log("error: ", error);
+    res.status(500).send({
+      error: error,
+    });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    let data = await User.findById(req.params?.id);
+    const response = data ? data : {};
+    console.log("user detail", JSON.stringify(response));
+    res.status(200).send(response);
+  } catch (error) {
+    console.log("error: ", error);
+    res.status(500).send({ error });
+  }
 });
 
 module.exports = router;
